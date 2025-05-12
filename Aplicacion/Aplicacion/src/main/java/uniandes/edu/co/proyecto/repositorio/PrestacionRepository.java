cf52833e8ac59d57f444ca30832c72adb7c45b4f
crissortiz <cd.ortizs12@uniandes.edu.co>
2025-05-11 22:36:35 -0500

    Creacion PrestacionRepository

---
+++ Aplicacion/Aplicacion/src/main/java/uniandes/edu/co/proyecto/repositorio/PrestacionRepository.java
@@ -0,0 +1,28 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import uniandes.edu.co.proyecto.modelo.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Collection;
+
+public interface PrestacionRepository extends JpaRepository<Prestacion, PrestacionPK> { // Usa la clase de clave primaria compuesta
+
+    @Query(value = "SELECT * FROM Prestacion", nativeQuery = true)
+    Collection<Prestacion> findAllPrestacion();
+    
+    @Query(value = "SELECT * FROM Prestacion WHERE IPS_nit = :nit AND ServicioSalud_idServicio = :idServicio", nativeQuery = true)
+    Prestacion findPrestacionById(@Param("nit") String nit, @Param("idServicio") Integer idServicio);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO Prestacion (IPS_nit, ServicioSalud_idServicio, Medico_registroMedico) VALUES (:nit, :idServicio, :registroMedico)", nativeQuery = true)
+    void createPrestacion(@Param("nit") String nit, @Param("idServicio") Integer idServicio, @Param("registroMedico") Integer registroMedico);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM Prestacion WHERE IPS_nit = :nit AND ServicioSalud_idServicio = :idServicio", nativeQuery = true)
+    void deletePrestacion(@Param("nit") String nit, @Param("idServicio") Integer idServicio);
+}
